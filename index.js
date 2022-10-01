a23df82acc26c985baca1974fd8b23ae2f810e1e
Kuldeep Pandey <kuldeep.pandey@spyne.ai>
2022-10-01 23:57:40 +0530

    basic boilerplate done

--- index.js
+++ index.js
@@ -4,9 +4,28 @@ const mongoose = require('mongoose')
 const morgan = require('morgan')
 const helmet = require('helmet')
 
-
+dotenv.config()
+//express server setup
 const app = express();
+
+//database connection
+mongoose.connect(process.env.MONGO_URL,() => {
+    console.log("Database connection successful")
+})
+
+
+//middlewares
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
 
 app.listen(8800,() => {
     console.log("fanfield backend is up and running!")
+})
+
+//mongodb database connection
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome to fanfield!")
 })
